8a9439616df088fa5312e2d6b8b95ecf56c1e2f8
fordgoat <adaadaajapan@gmail.com>
2024-06-11 00:34:33 +0700

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
 UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", "master")
 
 # Database
-DB_URI = os.environ.get("DATABASE_URL", "postgres://ljffexrs:nQNYkbGGeI-2-kHqoXJoL_ybuogzaxRl@trumpet.db.elephantsql.com/ljffexrs")
+DB_URI = os.environ.get("DATABASE_URL", "postgres://bmmrbtgd:C3aLXivbK9ZomPRSJ-0RU_p37CWfIMOl@raja.db.elephantsql.com/bmmrbtgd")
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001955926976"))
